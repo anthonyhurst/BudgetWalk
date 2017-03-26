5ca6d4cc84dfccb5532f13582633ec26cdc51319
Anthony Hurst <Anthony.Hurst@gmail.com>
2017-03-26 16:40:19 +0000

    BudgetWalker: Minimum Balance and Nicer Graph
    Show nicer y axis and x axis values for graph.  Except on minimum
    balance violation.

--- BudgetWalker.py
+++ BudgetWalker.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import datetime
 import pytz
+import numpy
 import pandas
 import matplotlib
 matplotlib.use('Agg')
@@ -59,25 +60,47 @@ def budgetWalk(config):
 
     dayDelta = datetime.timedelta(days=1)
     daysToShow = 30 if not 'DaysToShow' in config else config['DaysToShow']
+    minBalance = 0 if not 'MinimumBalance' in config else config['MinimumBalance']
 
     dfRecords = []
 
     for i in range(daysToShow):
         if local.day in instructions:
-            print(local.strftime("%Y-%m-%d"), end="\t")
+            print(local.strftime('%Y-%m-%d'), end="\t")
             balance += instructions[local.day]
             balance = round(balance,2)
-            print(balance)
-            dfRecords.append([local, balance])
+            if balance < minBalance:
+                raise Exception("Current balance goes below minimum: $" + str(balance) + " on " + str(local.strftime("%Y-%m-%d")))
+            print("${:,.2f}".format(balance))
+        dfRecords.append([pandas.to_datetime(local.strftime('%Y-%m-%d'), format='%Y-%m-%d'), balance])
         local += dayDelta
 
+
+    xticks = pandas.date_range(start=dfRecords[0][0], end=dfRecords[-1][0])
+
     df = pandas.DataFrame.from_records(dfRecords)
-    ax = df.plot()
+    df.index = xticks 
+    df.columns = ['Date', 'Balance']
+
+    ax = df.plot(figsize=(20,10))
+
     figure = ax.get_figure()
+
+    ax.set_ylabel('Balance')
+    ax.set_xlabel('Date')
+    ax.set_title('BudgetWalk')
+
+    ax.grid(True, which='major')
+    ax.yaxis.grid(True, which='major')
+
+    yvals = ax.get_yticks()
+    ax.set_yticklabels(['${:,.2f}'.format(x) for x in yvals])
+
+    ax.xaxis.set_minor_locator(matplotlib.dates.WeekdayLocator(byweekday=(1), interval=1))
+    ax.xaxis.set_minor_formatter(matplotlib.dates.DateFormatter('%m-%d'))
+
     figure.savefig(config['GraphOutputFile'])
     
-    
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Walk a budget to visualize current balance')
     parser.add_argument('--config', dest='config', default='config.json', help='Configuration file to use')

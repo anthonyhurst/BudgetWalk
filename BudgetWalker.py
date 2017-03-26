4a3845ec5732140bff4156a120c8be421d3a8c4f
Anthony Hurst <Anthony.Hurst@gmail.com>
2017-03-26 00:56:21 +0000

    BudgetWalker: Add CSV and JSON Parsers
    Add functions to easily read CSV and JSON files.  CSV reader optionally
    can type columns.

--- BudgetWalker.py
+++ BudgetWalker.py
@@ -1,12 +1,44 @@
 import argparse
+import json
 
+def readFile(filename):
+    f = open(filename, 'r')
+    content = f.read()
+    f.close()
+    return content
 
+def readJson(filename):
+    return json.loads(readFile(filename))
+
+def readCsv(filename, types=[]):
+    header = None
+    records = []
+    for line in readFile(filename).split("\n"):
+        if not line.strip(): # skip empty lines
+            continue
+        if header == None:
+            header = line.split(",")
+            continue
+        i = 0
+        record = {}
+        for column in line.split(","):
+            if len(types): # strongly typed if types available
+                column = types[i](column)
+            record[header[i]] = column
+            i+=1
+        records.append(record)
+    return records
+        
+            
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Walk a budget to visualize current balance')
     parser.add_argument('--config', dest='config', default='config.json', help='Configuration file to use')
     parser.add_argument('--budgetCsv', dest='budgetCsv', default='budget.csv', help='Budget CSV file')
-    parser.add_argument('--start', dest='start', default=0.00, type='float', help='Starting balance')
+    parser.add_argument('--start', dest='start', default=0.00, type=float, help='Starting balance')
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    
+    print(readJson(args.config))
+    print(readCsv(args.budgetCsv, types=[int, float])) 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BudgetWalk
 
-BudgetWalk allows you to visualize your balance over time with only a starting balance and a budget plan.  The budget CSV is currently as simple as a 'dayOfMonth' and a 'delta'.  Each line instructs the balance to change by the specified delta value on the specified day of the month.
+BudgetWalk allows you to visualize your balance over time with only a starting balance and a budget plan.  The budget CSV is currently as simple as a `dayOfMonth` and a `delta`.  Each line instructs the balance to change by the specified delta value on the specified day of the month.
 
 ## Example
 
